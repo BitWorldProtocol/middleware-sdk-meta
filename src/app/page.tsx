22e5fbdfe6d18da1b9e35f9db58822f804a754df
sunShineLoveMe <juneandrao@outlook.com>
2024-05-25 11:34:08 +0800

    build first page with TW

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
 import { Button } from "@/components/Button";
-import Image from "next/image";
+import { Input } from "@/components/Input";
+import { Textarea } from "@/components/Textarea";
 
 export default function Home() {
-  return <div>Root Page<Button variant="ghost">click me</Button></div>
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <form className="w-full max-w-md flex flex-col gap-4">
+        <h1 className="text-center text-2xl font-bold">Create App</h1>
+        <Input name="name" placeholder="App Name"></Input>
+        <Textarea name="description" placeholder="Description"></Textarea>
+        <Button type="submit">Submit</Button>
+      </form>
+    </div>
+  )
 }
