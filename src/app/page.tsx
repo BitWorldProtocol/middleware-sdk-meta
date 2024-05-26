2aad81501ec3dec54cd9c00660aea773ce20b29b
sunShineLoveMe <juneandrao@outlook.com>
2024-05-26 15:41:33 +0800

    db config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
 import { Textarea } from "@/components/Textarea";
+import { db } from "@/server/db/db";
 
-export default function Home() {
+export default async function Home() {
+  
+  const users = await db.query.Users.findMany()
+  
   return (
     <div className="h-screen flex justify-center items-center">
       <form className="w-full max-w-md flex flex-col gap-4">
