ee4821ea3271c4a6ecdcbea06ead11b7a732f2ff
sunShineLoveMe <juneandrao@outlook.com>
2024-05-26 11:30:46 +0800

    configure orm db

---
+++ drizzle.config.ts
@@ -0,0 +1,16 @@
+// import '@/drizzle/envConfig';
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./src/server/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    host: "localhost",
+    port: 5432,
+    user: "postgres",
+    password: "123456",
+    database: "postgres",
+  },
+  verbose: true,
+  strict: true,
+});
