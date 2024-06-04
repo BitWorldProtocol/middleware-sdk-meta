f11d9a64f0d5505c0ff293faa92a7a349c288553
sunShineLoveMe <juneandrao@outlook.com>
2024-06-04 10:28:51 +0800

    preview upload pic

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,14 @@
 "use client";
 import { useEffect, useState } from "react";
-import { UploadSuccessCallback, Uppy } from "@uppy/core";
+import {
+  UploadCallback,
+  UploadSuccessCallback,
+  Uppy,
+  UppyFile,
+} from "@uppy/core";
 import AWSS3 from "@uppy/aws-s3";
 import { useUppyState } from "./useUppyState";
-import { trpcClientReact, trpcPureClient } from "@/utils/api";
+import { trpcClient, trpcClientReact, trpcPureClient } from "@/utils/api";
 import { UploadButton } from "@/components/feature/UploadButton";
 import { Button } from "@/components/ui/Button";
 import Image from "next/image";
@@ -31,9 +36,13 @@ export default function Home() {
     return uppy;
   });
 
-  // const files = useUppyState(uppy, (s) => Object.values(s.files));
-  // 上传进度
-  // const progress = useUppyState(uppy, (s) => s.totalProgress);
+  const utils = trpcClientReact.useUtils();
+
+  const { data: fileList, isPending } =
+    trpcClientReact.file.listFiles.useQuery();
+
+  const [uploadingFileIDs, setUploadingFileIDs] = useState<string[]>([]);
+  const uppyFiles = useUppyState(uppy, (s) => s.files);
 
   useEffect(() => {
     const handler: UploadSuccessCallback<{}> = (file, resp) => {
@@ -42,28 +51,47 @@ export default function Home() {
           name: file.data instanceof File ? file.data.name : "test",
           path: resp.uploadURL ?? "",
           type: file.data.type,
+        }).then((res) => {
+          utils.file.listFiles.setData(void 0, (prev) => {
+            if (!prev) {
+              return prev
+            }
+            return [res, ...prev]
+          })
         });
       }
     };
+
+    const uploadProgressHandler: UploadCallback = (data) => {
+      setUploadingFileIDs((currentFiles) => [...currentFiles, ...data.fileIDs]);
+    };
+
+    const completeHandler = () => {
+      setUploadingFileIDs([]);
+    };
+
+    uppy.on("upload", uploadProgressHandler);
+
     uppy.on("upload-success", handler);
+
+    uppy.on("complete", completeHandler);
 
     return () => {
       uppy.off("upload-success", handler);
+      uppy.off("upload", uploadProgressHandler);
+      uppy.off("complete", completeHandler);
     };
   }, [uppy]);
 
-  const { data: fileList, isPending } =
-    trpcClientReact.file.listFiles.useQuery();
-  
   usePasteFile({
     onFilesPaste: (files) => {
       uppy.addFiles(
         files.map((file) => ({
-          data: file
-        })),
+          data: file,
+        }))
       );
     },
-  });  
+  });
 
   return (
     <div className="container mx-auto p-2">
@@ -81,14 +109,43 @@ export default function Home() {
       <Dropzone uppy={uppy}>
         {(draging) => {
           return (
-            <div className={cn("flex flex-wrap gap-4 relative", draging && "border border-dashed")}>
-              {
-                draging && (
-                  <div className="absolute inset-0 bg-secondary/30 flex justify-center items-center text-3xl ">
-                    Drop File Here to Upload
-                  </div>
-                )
-              }
+            <div
+              className={cn(
+                "flex flex-wrap gap-4 relative",
+                draging && "border border-dashed"
+              )}
+            >
+              {draging && (
+                <div className="absolute inset-0 bg-secondary/30 flex justify-center items-center text-3xl ">
+                  Drop File Here to Upload
+                </div>
+              )}
+
+              {uploadingFileIDs.length > 0 && uploadingFileIDs.map((id) => {
+                
+                  const file = uppyFiles[id];
+                  const isImage = file.data.type.startsWith("image");
+
+                  const url = URL.createObjectURL(file.data);
+                  return (
+                    <div
+                      key={file.id}
+                      className="w-56 h-56 flex justify-center items-center border border-red-500"
+                    >
+                      {isImage ? (
+                        <img src={url} alt={file.name} />
+                      ) : (
+                        <Image
+                          src="/unknown-file-types.png"
+                          width={100}
+                          height={100}
+                          alt="unknow file type"
+                        ></Image>
+                      )}
+                    </div>
+                  );
+                })}
+
               {fileList?.map((file) => {
                 const isImage = file.contentType.startsWith("image");
                 return (
@@ -114,10 +171,6 @@ export default function Home() {
         }}
       </Dropzone>
       <UploadPreview uppy={uppy}></UploadPreview>
-      {/* {files.map((file) => {
-        const url = URL.createObjectURL(file.data);
-        return <img src={url} key={file.id} alt={file.name}/>;
-      })} */}
     </div>
   );
 }
